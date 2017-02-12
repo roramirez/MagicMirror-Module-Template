3a30238fa18b658d3857df4bd22d04fdb3057d87
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2017-02-11 21:46:23 -0300

    Add create script module

---
+++ create_module.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# This script create a module in base e MagicMirror-Module template
+# You can get a copy from https://github.com/roramirez/MagicMirror-Module-Template
+#
+#
+# Manager Module Template
+#
+# By Rodrigo Ramìrez Norambuena https://rodrigoramirez.com
+# MIT Licensed.
+#
